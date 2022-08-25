dee1e6f55bdafeb0ccae6f34e60a93f02970b689
--replace-all <--replace-all>
2022-08-25 19:32:06 +0300

    set methods quiz instructions

--- ruby-methods/methods.rb
+++ ruby-methods/methods.rb
@@ -6,3 +6,16 @@
 ## Q3 Write a method where var1 and var2 are the parameters. Call the method. The statements to be executed are:
 #"Phase-1 was all about Introduction to Programming."
 #"Phase-2 was all about React."
+
+##Q4 Write a Ruby program which accept the user's first and last name and print them in reverse order with a 
+#space between them.
+
+## Q5 Ruby program to illustrate the passing of parameters to a method
+# Output 
+#ID is: 1
+#Color is: Red
+#Name is: ABC
+
+#ID is: 2
+#Color is: Black
+#Name is: XYZ
