ea1a18260c86cdf70a78ba0eda82798cd842f014
--replace-all <--replace-all>
2022-08-25 19:02:23 +0300

    set variables and data type quiz instructions

---
+++ variables-and-data-types/app.rb
@@ -0,0 +1,11 @@
+# terminal output methods
+## Q1 Set variables for the corresponding values:
+
+#a number to the value 25
+#a boolean to the value true 
+#a string to the value "Ruby"
+
+##  Q2 Set values 100, 30 for the  variables cars and drivers respectively.
+#Output: There are 100 cars and 30 drivers.
+
+
