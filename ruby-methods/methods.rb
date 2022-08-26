7be994fa8998cb87afde0a3acf2b9106ec1fd247
PatrickKibe <phantickibe01@gmail.com>
2022-08-26 16:31:05 +0300

    initial commit

--- ruby-methods/methods.rb
+++ ruby-methods/methods.rb
@@ -1,29 +1,36 @@
+
 # Ruby Methods
 ## Q1 Write any simple Ruby Program to illustrate the defining and calling of method
 
-def say_hi (name = "Andrew")
-    puts "Hi! #{name}"
+evil_monster = "Bowser"
+
+def princess_peaches_castle (evil_monster)
+    puts"#{evil_monster} is trying to kidnap princess peach!"
 end
 
-say_hi = Andrew
-
-
-
-
-
+princess_peaches_castle (evil_monster)
 
 ## Q2 You have a string, "I love espresso", return the length of the string
-
-
-
-
+favourite = "I love espresso"
+favourite.length    
+end
 
 ## Q3 Write a method where var1 and var2 are the parameters. Call the method. The statements to be executed are:
 #"Phase-1 was all about Introduction to Programming."
 #"Phase-2 was all about React."
 
+my_hash = { var1: "Phase-1 was all about Introduction to Programming.", 
+            var2: "Phase-2 was all about React." }
+my_hash[:var2]
+
+
 ##Q4 Write a Ruby program which accept the user's first and last name and print them in reverse order with a 
-#space between them.
+#space between them
+
+def user (firstname, lastname)
+    print_user.reverse
+end
+
 
 ## Q5 Ruby program to illustrate the passing of parameters to a method
 # Output 
@@ -31,9 +38,16 @@ say_hi = Andrew
 #Color is: Red
 #Name is: ABC
 
+
+
+
 #ID is: 2
 #Color is: Black
 #Name is: XYZ
+
+
+
+
 
 
 # nimeongeza hii line
--- variables-and-data-types/app.rb
+++ variables-and-data-types/app.rb
@@ -5,7 +5,12 @@
 #a boolean to the value true 
 #a string to the value "Ruby"
 
+def Variables ("number = 25", "boolean = True", "string = Ruby")
+    @number = "25"
+    @boolean
+    @string
+end
+
 ##  Q2 Set values 100, 30 for the  variables cars and drivers respectively.
 #Output: There are 100 cars and 30 drivers.
-
 
