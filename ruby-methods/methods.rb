7cf6d22925b8f4dd517af3dd63f79d27740d0f76
Wangari-Sharon <wangarik35@gmail.com>
2022-08-26 13:30:32 +0300

    Q2 Done

--- ruby-methods/methods.rb
+++ ruby-methods/methods.rb
@@ -7,10 +7,9 @@ end
 
 ## Q2 You have a string, "I love espresso", return the length of the string
 
-def output_string(value)
-    puts value.length
+def output_string("I love espresso")
+    puts "I love espresso".length
 end
-puts output_string("I love espresso")
 
 ## Q3 Write a method where var1 and var2 are the parameters. Call the method. The statements to be executed are:
 #"Phase-1 was all about Introduction to Programming."
